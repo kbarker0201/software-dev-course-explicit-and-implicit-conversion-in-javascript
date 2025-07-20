d3c075f573d382d59e4011b940ae9164a6e3cb4a
kbarker0201 <kbarker0201@gmail.com>
2025-07-19 22:55:28 -0500

    final changes

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,32 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = Number("5") - 2;
+/* Add the Number() method in the above line of code to change 5 to a number so it can
+be subtracted from */
 console.log("The result is: " + result);
 
 let isValid = Boolean("false");
+/* Here, the Boolean() method is already used to convert the string to a boolean so it
+can be properly used in the next line of code */
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+let age = Number("25");
+/* Add the Number() method here to change age to a number rather than a string so it 
+can be used in the mathematical formula that comes in the next line */
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+let numCattle = "13";
+let numSheep = 24;
+let totalLivestock = Number(numCattle) + numSheep;
+
+console.log(totalLivestock);
+
+let tulipBouqets = 30;
+let roseBouqets = "35";
+let totalBouqets = tulipBouqets = Number(roseBouqets);
+
+console.log("Total number of bouqets: " + 25);
